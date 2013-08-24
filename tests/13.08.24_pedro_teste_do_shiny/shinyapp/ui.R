dcce56e7f47695009ca569778221957e5ad141ec
pedroliman <pedrolima.n@gmail.com>
2013-08-24 14:02:52 -0300

    Testes com o shiny

---
+++ tests/13.08.24_pedro_teste_do_shiny/shinyapp/ui.R
@@ -0,0 +1,12 @@
+library(shiny)
+
+# Define UI for miles per gallon application
+shinyUI(pageWithSidebar(
+  
+  # Application title
+  headerPanel("Uniconf - Analise de Confiabilidade"),
+  
+  sidebarPanel(),
+  
+  mainPanel()
+))
